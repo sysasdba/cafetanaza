287b4e2f655f9673be6df4a9a706b9a0204f77dc
sysasdba <912havilah@gmail.com>
2023-08-16 16:15:10 +0700

    rapihin welcome

--- resources/views/reservations/step-one.blade.php
+++ resources/views/reservations/step-one.blade.php
@@ -4,8 +4,7 @@
             <div class="flex-1 h-full max-w-4xl mx-auto bg-white rounded-lg shadow-xl">
                 <div class="flex flex-col md:flex-row">
                     <div class="h-32 md:h-auto md:w-1/2">
-                        <img class="object-cover w-full h-full"
-                            src="https://cdn.pixabay.com/photo/2017/07/26/21/14/spilled-coffee-2543133_1280.jpg" alt="img" />
+                        <img class="object-cover w-full h-full" src="https://cdn.pixabay.com/photo/2017/07/26/21/14/spilled-coffee-2543133_1280.jpg" alt="img" />
                     </div>
                     <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
                         <div class="w-full">
@@ -22,35 +21,29 @@
                                     <label for="first_name" class="block text-sm font-medium text-gray-700"> First Name
                                     </label>
                                     <div class="mt-1">
-                                        <input type="text" id="first_name" name="first_name" maxlength="15"
-                                            value="{{ $reservation->first_name ?? '' }}"
-                                            class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                        <input type="text" id="first_name" name="first_name" maxlength="20" value="{{ $reservation->first_name ?? '' }}" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                     </div>
                                     @error('first_name')
-                                        <div class="text-sm text-red-400">{{ $message }}</div>
+                                    <div class="text-sm text-red-400">{{ $message }}</div>
                                     @enderror
                                 </div>
                                 <div class="sm:col-span-6">
                                     <label for="last_name" class="block text-sm font-medium text-gray-700"> Last Name
                                     </label>
                                     <div class="mt-1">
-                                        <input type="text" id="last_name" name="last_name" maxlength="15"
-                                            value="{{ $reservation->last_name ?? '' }}"
-                                            class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                        <input type="text" id="last_name" name="last_name" maxlength="20" value="{{ $reservation->last_name ?? '' }}" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                     </div>
                                     @error('last_name')
-                                        <div class="text-sm text-red-400">{{ $message }}</div>
+                                    <div class="text-sm text-red-400">{{ $message }}</div>
                                     @enderror
                                 </div>
                                 <div class="sm:col-span-6">
                                     <label for="email" class="block text-sm font-medium text-gray-700"> Email </label>
                                     <div class="mt-1">
-                                        <input type="email" id="email" name="email" maxlength="20"
-                                            value="{{ $reservation->email ?? '' }}"
-                                            class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                        <input type="email" id="email" name="email" maxlength="50" value="{{ $reservation->email ?? '' }}" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                     </div>
                                     @error('email')
-                                        <div class="text-sm text-red-400">{{ $message }}</div>
+                                    <div class="text-sm text-red-400">{{ $message }}</div>
                                     @enderror
                                 </div>
                                 <div class="sm:col-span-6">
@@ -58,12 +51,11 @@
                                         number
                                     </label>
                                     <div class="mt-1">
-                                        <input type="text" id="tel_number" name="tel_number" maxlength="13"
-                                            value="{{ $reservation->tel_number ?? '' }}"
-                                            class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                        <input type="text" id="tel_number" name="tel_number" maxlength="20" value="{{ $reservation->tel_number ?? '' }}" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                     </div>
+                                    <span class="text-xs">(+62851xxxxxxxx)</span>
                                     @error('tel_number')
-                                        <div class="text-sm text-red-400">{{ $message }}</div>
+                                    <div class="text-sm text-red-400">{{ $message }}</div>
                                     @enderror
                                 </div>
                                 <div class="sm:col-span-6">
@@ -71,15 +63,11 @@
                                         Date
                                     </label>
                                     <div class="mt-1">
-                                        <input type="datetime-local" id="res_date" name="res_date"
-                                            min="{{ $min_date->format('Y-m-d\TH:i:s') }}"
-                                            max="{{ $max_date->format('Y-m-d\TH:i:s') }}"
-                                            value="{{ $reservation ? $reservation->res_date->format('Y-m-d\TH:i:s') : '' }}"
-                                            class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                        <input type="datetime-local" id="res_date" name="res_date" min="{{ $min_date->format('Y-m-d\TH:i:s') }}" max="{{ $max_date->format('Y-m-d\TH:i:s') }}" value="{{ $reservation ? $reservation->res_date->format('Y-m-d\TH:i:s') : '' }}" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                     </div>
                                     <span class="text-xs">Please choose the time between 07:00-22:00.</span>
                                     @error('res_date')
-                                        <div class="text-sm text-red-400">{{ $message }}</div>
+                                    <div class="text-sm text-red-400">{{ $message }}</div>
                                     @enderror
                                 </div>
                                 <div class="sm:col-span-6">
@@ -87,17 +75,14 @@
                                         Number
                                     </label>
                                     <div class="mt-1">
-                                        <input type="number" id="guest_number" name="guest_number" min="10" max="40"
-                                            value="{{ $reservation->guest_number ?? '' }}"
-                                            class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                        <input type="number" id="guest_number" name="guest_number" min="10" max="40" value="{{ $reservation->guest_number ?? '' }}" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                     </div>
                                     @error('guest_number')
-                                        <div class="text-sm text-red-400">{{ $message }}</div>
+                                    <div class="text-sm text-red-400">{{ $message }}</div>
                                     @enderror
                                 </div>
                                 <div class="mt-6 p-4 flex justify-end">
-                                    <button type="submit"
-                                        class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Next</button>
+                                    <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Next</button>
                                 </div>
                             </form>
                         </div>
